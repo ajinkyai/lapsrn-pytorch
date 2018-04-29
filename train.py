5497af03f7cbbfb53d51043590215c7299fce2bb
Ajinkya Indulkar <ajinkya.anant.indulkar@gmail.com>
2018-04-29 11:23:54 -0400

    Add nIters param

--- train.py
+++ train.py
@@ -27,10 +27,11 @@ parser.add_argument('--batchSize', type=int, default=16, help='training batch si
 parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
 parser.add_argument('--nEpochs', type=int, default=50, help='number of epochs to train for')
 parser.add_argument('--checkpoint', type=str, default='./model', help='Path to checkpoint')
-parser.add_argument('--lr', type=float, default=0.001, help='Learning Rate. Default=0.01')
+parser.add_argument('--lr', type=float, default=0.0001, help='Learning Rate. Default=0.01')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
 parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+parser.add_argument('--nIters', type=int, default=50, help='Number of iterations in epoch')
 opt = parser.parse_args()
 
 print(opt)
@@ -66,12 +67,13 @@ if cuda:
     criterion = criterion.cuda()
 
 
-
+# 50
 losses = []
 
 def train(epoch):
-    
-    for i in range(150):
+    NITERS = opt.nIters
+    avg_loss = 0
+    for i in range(NITERS):
         epoch_loss = 0
         for iteration, batch in enumerate(training_data_loader, 1):
             LR, HR_2_target, HR_4_target = Variable(batch[0]), Variable(batch[1]), Variable(batch[2])
@@ -90,15 +92,19 @@ def train(epoch):
             # loss3 = CharbonnierLoss(HR_8, HR_8_target)
             loss3 = 0
             loss = loss1+loss2+loss3   
-            losses.append(loss)
+            avg_loss += loss
             epoch_loss += loss.data[0]
             loss.backward()
             optimizer.step()
     
             #print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(training_data_loader), loss.data[0]))
         print("===> Epoch {}, Loop{}: Avg. Loss: {:.4f}".format(epoch, i, epoch_loss / len(training_data_loader)))
+        avg_loss += epoch_loss / len(training_data_loader)
+    avg_loss /= NITERS
+    losses.append(avg_loss)
+        
 
-
+psnrs = []
 def test():
     avg_psnr1 = 0
     avg_psnr2 = 0
@@ -116,14 +122,16 @@ def test():
         mse1 = criterion(HR_2, HR_2_target)
         mse2 = criterion(HR_4, HR_4_target)
         # mse3 = criterion(HR_8, HR_8_target)        
-        psnr1 = 10 * log10(1 / mse1.data[0])
-        psnr2 = 10 * log10(1 / mse2.data[0])
+        psnr1 = 10 * log10(1 / mse1.item())
+        psnr2 = 10 * log10(1 / mse2.item())
         # psnr3 = 10 * log10(1 / mse3.data[0])
         avg_psnr1 += psnr1
         avg_psnr2 += psnr2
+        
         # avg_psnr3 += psnr3
     print("===> Avg. PSNR1: {:.4f} dB".format(avg_psnr1 / len(testing_data_loader)))
     print("===> Avg. PSNR2: {:.4f} dB".format(avg_psnr2 / len(testing_data_loader)))
+    psnrs.append(avg_psnr2 / len(testing_data_loader))
     # print("===> Avg. PSNR3: {:.4f} dB".format(avg_psnr3 / len(testing_data_loader)))
 
 
@@ -136,7 +144,7 @@ def checkpoint(epoch):
 
 lr=opt.lr
 for epoch in range(1, opt.nEpochs + 1):
-    optimizer = optim.Adam(model.parameters(), lr=opt.lr, weight_decay=1e-5)
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr, weight_decay=1e-4)
     train(epoch)
     test()
     if epoch % 10 ==0:
@@ -144,3 +152,4 @@ for epoch in range(1, opt.nEpochs + 1):
         print('new learning rate {}'.format(lr))
         checkpoint(epoch)
 np.save('losses', losses)
+np.save('psnrs', psnrs)
